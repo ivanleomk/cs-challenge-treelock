0a071989ac69ef0b134f81867f1c072b252fbcbf
Mark Yuen <markyuen11@gmail.com>
2021-09-25 16:58:55 +0800

    ttt10

--- main.py
+++ main.py
@@ -45,26 +45,30 @@ async def run_ttt(request: Request):
     board = ttt.initial_state()
     for event in client.events():
         body = json.loads(event.data)
-        print(body)
+        print("RECEIVED", body)
         # First initiation
         if "youAre" in body:
             if body["youAre"] == "O":
                 action = ttt.minimax(board)
                 board = ttt.result(board, action)
+                print("SENT", {"action": "putSymbol", "position": ttt.get_dir(action)})
                 requests.post(
                     url, json={"action": "putSymbol", "position": ttt.get_dir(action)})
             continue
         # Other msgs from server
-        # Execute the server's action
-        action = ttt.get_action(body["position"])
-        board = ttt.result(board, action)
-        # Execute and send back our own action
-        action = ttt.minimax(board)
-        board = ttt.result(board, action)
-        requests.post(
-            url, json={"action": "putSymbol", "position": ttt.get_dir(action)})
-    requests.post(url, json={"action": "(╯°□°)╯︵ ┻━┻"})
-    print("########")
+        if "position" in body:
+            # Execute the server's action
+            action = ttt.get_action(body["position"])
+            board = ttt.result(board, action)
+            # Execute and send back our own action
+            action = ttt.minimax(board)
+            board = ttt.result(board, action)
+            print("SENT", {"action": "putSymbol", "position": ttt.get_dir(action)})
+            requests.post(
+                url, json={"action": "putSymbol", "position": ttt.get_dir(action)})
+        else:
+            requests.post(url, json={"action": "(╯°□°)╯︵ ┻━┻"})
+    print("########\n\n")
 
 @app.post("/stonks")
 async def run_stonks(request: Request):
