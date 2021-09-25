fa0972d5a1d0a86944497d7278e74f1972c8e906
Koa Zhao Yuan <claydol345@hotmail.com>
2021-09-25 12:49:43 +0800

    credit sussie can use airbnb style guide?

--- main.py
+++ main.py
@@ -25,5 +25,5 @@ async def run_parasite(request: Request):
     from parasites import solve
     body = await request.body()
     body = json.loads(body)
-    return solve(body["test_cases"])
+    return solve(body)
     
