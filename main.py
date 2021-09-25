6be08f0cd0e4803d6d3067d0cb84e466aca52176
Mark Yuen <markyuen11@gmail.com>
2021-09-25 15:23:32 +0800

    ttt start?

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from typing import List
-
+import json, requests, os, sseclient
 
 # Instantiate the FastAPI
 app = FastAPI()
@@ -18,12 +18,19 @@ async def run_asteroid(request: Request):
     body = await request.body()
     return calc_asteroids(body)
 
-import json
-
 @app.post("/parasite")
 async def run_parasite(request: Request):
     from parasites import solve
     body = await request.body()
     body = json.loads(body)
     return solve(body)
-    
+
+@app.post("/tic-tac-toe")
+async def run_ttt(request: Request):
+    body = await request.body()
+    battle_id = body["battleId"]
+    stream = requests.get(os.path.join("https://cis2021-arena.herokuapp.com/tic-tac-toe/start", battle_id), stream=True)
+    client = sseclient.SSEClient(stream)
+    for event in client.events():
+        print(event.data)
+    print("########")
