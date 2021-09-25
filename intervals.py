cbb83d54aedca92b35a4251873b4f68c3ea9397f
Ivan Leo <ivanleomk@gmail.com>
2021-09-26 00:36:48 +0800

    Return acc instead

--- intervals.py
+++ intervals.py
@@ -1,4 +1,6 @@
-x = [[0,2],[3,6],[2,5],[2,12]]
+# x = [[0,2],[3,6],[2,5],[2,12]]
+
+x = [{'from': 717649920, 'to': 900723920}], [{'from': 323563706, 'to': 836556107}], [{'from': 96021970, 'to': 98042557}], [{'from': 180425524, 'to': 513015509}]
 
 def find_overlap(interval_1,interval_2):
     start_1,end_1 = interval_1
@@ -6,31 +8,29 @@ def find_overlap(interval_1,interval_2):
 
     return max(start_1,start_2),min(end_1,end_2)
 
+def generate_intervals(raw_data):
+    return [
+        [x[0]['from'],x[0]['to']] for x in raw_data
+    ]
+
 def find_overlapping_interval(intervals):
     #Sort by the front
     intervals.sort(key=lambda x: x[0])
-
-    lastSeen = intervals[0]
-    merging = False
+    res = [[1,intervals[0]]]
     
     for index,item in enumerate(intervals):
         if index == 0: continue
 
         start,end = item
-        prev_start,prev_end = lastSeen
+        _,prev_end = res[-1][1]
+        ctr = res[-1][0]
 
-        if merging:
-            #If we have found the end of an interval
-            if start > prev_end:
-                merging = False
-                return lastSeen
-            lastSeen = find_overlap(lastSeen,item)
-            continue
+        if start <= prev_end:
+            res[-1] = [ctr+1,find_overlap(item,res[-1][1])]
             
-        if prev_end <= start:
-            merging = True
-            lastSeen = item
+        else:
+            res.append([1,item])
 
-    return lastSeen
+    return res
 
-print(find_overlapping_interval(x))
+print(find_overlapping_interval(generate_intervals(x)))
