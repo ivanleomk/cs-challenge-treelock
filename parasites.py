7abc2329ac61198a23bbe408858707df9b38257f
Ivan Leo <ivanleomk@gmail.com>
2021-09-25 15:35:22 +0800

    removed print statements

--- parasites.py
+++ parasites.py
@@ -121,7 +121,6 @@ def solve_1_and_2_alt(data,interested):
       vacant+=1
 
     elif data[r][c] == 1:
-      print("Found infected cell of {},{}".format(r,c))
       infected +=1
       #Find time to infect first person
       if (r,c)!=(start_row,start_col) and first_infected == -1:
@@ -139,7 +138,6 @@ def solve_1_and_2_alt(data,interested):
 
     data[r][c] = '#'
   
-  print("Determined that we had {} infected and {} to be infected".format(infected,num))
   return tally,first_infected if num == infected else -1
 
 def solve_1_and_2(data, x, y):
@@ -191,11 +189,9 @@ def solve_1_and_2(data, x, y):
 
 def solve(dataArr):
     res = []
-    print(dataArr)
     for i in range(len(dataArr)):
         
         data = dataArr[i]
-        print(data)
         if data["room"] == 19:
             ans = {
                 "room": {},
@@ -220,5 +216,4 @@ def solve(dataArr):
             }
             res.append(ans)
 
-    print(res)
     return res
